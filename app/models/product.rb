054635acbe4046ece8ff15f069cbb6ada5af6da8
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2024-03-28 12:27:05 +0100

    Added categories and products show page for the user

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,6 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
+    attachable.variant :medium, resize_to_limit: [250, 250]
   end
 end
