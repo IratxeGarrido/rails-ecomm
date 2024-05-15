119f2beb5a00c6b31414232ebe5a15a4c9e623fc
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2024-05-15 12:10:43 +0200

    Filter categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products
+    @products = @products.where("price <= ?", params[:max]) if params[:max].present?
+    @products = @products.where("price >= ?", params[:min]) if params[:min].present?
   end
 end
